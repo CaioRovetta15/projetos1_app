775c097c71598abeeaa779e427c07cdf0b4e6837
calvinsuzuki <calvinssdcamargo@gmail.com>
2022-06-13 23:51:32 -0300

    dynamic access image implemented

--- demo_apresentacao_2/lib/FireStorageService.dart
+++ demo_apresentacao_2/lib/FireStorageService.dart
@@ -1,7 +1,6 @@
 //import 'dart:developer';
 // ignore_for_file: avoid_print
 
-import 'dart:io';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/cupertino.dart';
 
@@ -9,8 +8,8 @@ class FireStorageService extends ChangeNotifier {
   FireStorageService();
 
   static Future<String> loadImage(BuildContext context, String image) async {
-    var referencia = FirebaseStorage.instance.ref();
-    var url = referencia.child('/Gaveta1/dipirona.png').getDownloadURL();
+    var ref = FirebaseStorage.instance.ref();
+    var url = ref.child(image.substring(36)).getDownloadURL();
     return url;
   }
 }
--- demo_apresentacao_2/lib/accesDetail.dart
+++ demo_apresentacao_2/lib/accesDetail.dart
@@ -1,6 +1,4 @@
 // ignore_for_file: file_names, camel_case_types, non_constant_identifier_names, prefer_const_constructors
-import 'dart:developer';
-import 'dart:io';
 import 'package:demo_apresentacao_2/FireStorageService.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
@@ -18,12 +16,10 @@ class accesDetail extends StatelessWidget {
       url = value.toString();
     });
     return url;
-    //return 'https://firebasestorage.googleapis.com/v0/b/smartcabinet-a190c.appspot.com/o/Gaveta1%2Fsuperheolanding.gif?alt=media&token=72eb9fef-b492-4885-a387-f5ec90af03aa';
   }
 
   @override
   Widget build(BuildContext context) {
-    stdout.writeln("build: here");
     return Scaffold(
       appBar: AppBar(
         title: Text(access.operador),
@@ -34,9 +30,7 @@ class accesDetail extends StatelessWidget {
               future: _getImage(context, access.gifPath),
               builder: (context, snapshot) {
                 if (snapshot.hasData) {
-                  stdout.writeln("FutureBuilder: here");
                   return Image.network(snapshot.data.toString());
-                  //fit:Boxfit.fill;
                 } else {
                   return CircularProgressIndicator();
                 }
