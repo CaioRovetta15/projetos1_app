1b3719a9a8ce40ddc30bfa77bc163354711b8357
CaioRovetta15 <caiorovetta123@usp.br>
2022-06-13 23:40:05 -0300

    fixed for android

--- demo_apresentacao_2/lib/FireStorageService.dart
+++ demo_apresentacao_2/lib/FireStorageService.dart
@@ -1,4 +1,6 @@
 //import 'dart:developer';
+// ignore_for_file: avoid_print
+
 import 'dart:io';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/cupertino.dart';
@@ -6,28 +8,9 @@ import 'package:flutter/cupertino.dart';
 class FireStorageService extends ChangeNotifier {
   FireStorageService();
 
-  static Future<dynamic> loadImage(BuildContext context, String image) async {
-    print(image);
-    var referencia = FirebaseStorage.instance;
-    var path = referencia
-        .ref()
-        .child('gs://smartcabinet-a190c.appspot.com/Gaveta1/kablogers.png');
-    print(path.getDownloadURL().toString());
-    //return await ref.getDownloadURL();
-
-    final FirebaseStorage firebaseStorage =
-        FirebaseStorage(FirebaseStorage.instance.app, image);
-
-    return path
-        .getDownloadURL()
-        .toString(); // 'https://firebasestorage.googleapis.com/v0/b/smartcabinet-a190c.appspot.com/o/Gaveta1%2Fsuperheolanding.gif?alt=media&token=72eb9fef-b492-4885-a387-f5ec90af03aa';
-
-    //return ';
+  static Future<String> loadImage(BuildContext context, String image) async {
+    var referencia = FirebaseStorage.instance.ref();
+    var url = referencia.child('/Gaveta1/dipirona.png').getDownloadURL();
+    return url;
   }
-
-  //https://firebase.google.com/docs/storage/flutter/handle-errors
-
-  // static Future<dynamic> getImage(String imageUrl) async {
-  //   return FirebaseStorage.instance.refFromURL(imageUrl);
-  // }
 }
